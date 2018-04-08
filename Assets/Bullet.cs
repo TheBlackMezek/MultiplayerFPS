7a2ee3d53e0301bff837435f378827b2f3a490da
Alexander Thomas <s179069@students.aie.edu.au>
2018-04-07 17:03:01 -0700

    Tweak gameplay & build level

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -6,15 +6,29 @@ using UnityEngine.Networking;
 public class Bullet : NetworkBehaviour {
 
     public float damage = 10.0f;
+    public float lifetime = 5.0f;
     private bool hit = false;
+
+    private void Start()
+    {
+        Invoke("Kill", lifetime);
+    }
+
+    private void Kill()
+    {
+        Destroy(gameObject);
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(isServer && !hit && collision.transform.tag == "Player")
+        if(isServer && !hit)
         {
             hit = true;
-            PlayerController p = collision.transform.GetComponent<PlayerController>();
-            p.CmdDamage(damage);
+            if (collision.transform.tag == "Player")
+            {
+                PlayerController p = collision.transform.GetComponent<PlayerController>();
+                p.CmdDamage(damage);
+            }
             Destroy(gameObject);
         }
     }
