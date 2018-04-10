bd4e4439f066e71b17b2707d6ade0b3066b94893
Alexander Thomas <s179069@students.aie.edu.au>
2018-04-09 17:58:58 -0700

    Add tracert stuff

---
+++ Assets/NetStuff/NetNode.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetNode : MonoBehaviour {
+
+    public float posRange = 50.0f;
+
+    public byte[] ip;
+    public List<NetNode> connections;
+    public LineRenderer lr;
+
+    public string IpString
+    {
+        get
+        {
+            string s = "";
+
+            for (int i = 0; i < ip.Length - 1; ++i)
+            {
+                s += ip[i].ToString() + '.';
+            }
+            s += ip[ip.Length - 1].ToString();
+
+            return s;
+        }
+    }
+
+    public void RandPos()
+    {
+        transform.position = new Vector3(Random.Range(-posRange, posRange),
+                                         Random.Range(-posRange, posRange),
+                                         Random.Range(-posRange, posRange));
+    }
+
+    public void Setup()
+    {
+        
+        Color c = new Color(ip[0] / 255.0f,
+                            ip[1] / 255.0f,
+                            ip[2] / 255.0f,
+                            (((ip[3] / 255.0f) / 2.0f)) + 0.5f);
+    
+        Material mat = GetComponent<Renderer>().material;
+        mat.color = c;
+
+        lr.startColor = c;
+        lr.endColor = c;
+        lr.positionCount = connections.Count * 2 + 1;
+        lr.SetPosition(0, transform.position);
+        for(int i = 0; i < connections.Count; ++i)
+        {
+            lr.SetPosition(i * 2 + 1, connections[i].transform.position);
+            lr.SetPosition(i * 2 + 2, transform.position);
+        }
+    }
+
+}
---
+++ Assets/NetStuff/Tracert.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+
+public class Tracert : MonoBehaviour {
+
+    public string[] prog;
+    public string[] args;
+    public bool[] useArgs;
+    public GameObject nodePrefab;
+
+    private System.Diagnostics.Process p;
+    private List<NetNode> nodes = new List<NetNode>();
+    public string output;
+
+
+    void Start () {
+        for(int i = 0; i < prog.Length; ++i)
+        {
+            p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = prog[i];
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+
+            if (useArgs[i])
+            {
+                p.StartInfo.Arguments = args[i];
+            }
+
+            p.Start();
+
+
+            StartCoroutine("ReceiveOutput");
+        }
+        BuildNetModel();
+    }
+	
+	private IEnumerator ReceiveOutput()
+    {
+        output = p.StandardOutput.ReadToEnd();
+        AddTracertResults();
+        yield return null;
+    }
+
+    private void AddTracertResults()
+    {
+        string[] lines = output.Split();
+        NetNode lastNode = null;
+
+        for(int i = 0; i < lines.Length; ++i)
+        {
+            string l = lines[i];
+            if(l.Length > 0 && (l[0] == '[' || (!StrContainsLetters(l) && CountChars(l, '.') == 3)))
+            {
+                if(l[0] == '[')
+                {
+                    l = l.Remove(l.Length - 1, 1);
+                    l = l.Remove(0, 1);
+                }
+
+                NetNode node = FindNode(l);
+                if(node == null)
+                {
+                    GameObject obj = Instantiate(nodePrefab);
+                    node = obj.GetComponent<NetNode>();
+                    node.ip = new byte[4];
+                    for (int n = 0; n < 3; ++n)
+                    {
+                        int idx = l.IndexOf('.');
+                        node.ip[n] = (byte)int.Parse(l.Substring(0, idx));
+                        l = l.Remove(0, idx + 1);
+                    }
+                    node.ip[3] = (byte)int.Parse(l);
+                    nodes.Add(node);
+                }
+
+                if(lastNode != null)
+                {
+                    node.connections.Add(lastNode);
+                }
+
+                lastNode = node;
+            }
+        }
+        
+    }
+
+    private void BuildNetModel()
+    {
+        foreach (NetNode n in nodes)
+        {
+            n.RandPos();
+        }
+        foreach (NetNode n in nodes)
+        {
+            n.Setup();
+        }
+    }
+
+    private NetNode FindNode(string ip)
+    {
+        foreach (NetNode n in nodes)
+        {
+            if (n.IpString == ip)
+            {
+                return n;
+            }
+        }
+        return null;
+    }
+
+    private bool StrContainsLetters(string str)
+    {
+        bool ret = false;
+
+        str = str.ToLower();
+        string[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+                             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+        foreach(string s in alphabet)
+        {
+            if(str.Contains(s))
+            {
+                ret = true;
+                break;
+            }
+        }
+
+        return ret;
+    }
+
+    private int CountChars(string str, char chr)
+    {
+        int ret = 0;
+
+        foreach(char c in str)
+        {
+            if(c == chr)
+            {
+                ++ret;
+            }
+        }
+
+        return ret;
+    }
+
+}
