1a687a40efbb93aed064a8278a6c7c17115ccb75
Alexander Thomas <s179069@students.aie.edu.au>
2018-04-10 18:00:54 -0700

    Organize stuff & add WWW scene

---
+++ Assets/NetStuff/WWW/ImageTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageTest : MonoBehaviour {
+
+    public Renderer renderer;
+
+
+
+    private void Start()
+    {
+        string url = "https://i.redd.it/kbrpe1rsk3r01.jpg";
+        WWW www = new WWW(url);
+        StartCoroutine(GetImage(www));
+    }
+
+    public IEnumerator GetImage(WWW www)
+    {
+        yield return www;
+        
+        renderer.material.mainTexture = www.texture;
+    }
+
+}
