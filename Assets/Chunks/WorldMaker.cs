f89df98269a65ab7d0b1d6a4ed8115b8dc7b3be9
Alexander Thomas <s179069@students.aie.edu.au>
2018-04-17 11:49:19 -0700

    Implement chunk face culling

--- Assets/Chunks/WorldMaker.cs
+++ Assets/Chunks/WorldMaker.cs
@@ -5,25 +5,66 @@ using UnityEngine;
 public class WorldMaker : MonoBehaviour
 {
 
-    public Mesh cubeMesh;
+    //public Mesh cubeMesh;
     public GameObject chunkPrefab;
 
     public int chunkSize = 16;
     public int worldSize = 3;
 
 
+    private Vector3[] cubeVertices =
+    {
+        new Vector3(1, 0, 1), new Vector3(0, 0, 1), new Vector3(0, 1, 1), new Vector3(1, 1, 1), //front
+        new Vector3(1, 1, 0), new Vector3(0, 1, 0), new Vector3(0, 0, 0), new Vector3(1, 0, 0), //back
+        new Vector3(0, 1, 0), new Vector3(1, 1, 0), new Vector3(1, 1, 1), new Vector3(0, 1, 1), //top
+        new Vector3(1, 0, 0), new Vector3(0, 0, 0), new Vector3(0, 0, 1), new Vector3(1, 0, 1), //bot
+        new Vector3(1, 0, 0), new Vector3(1, 0, 1), new Vector3(1, 1, 1), new Vector3(1, 1, 0), //right
+        new Vector3(0, 0, 1), new Vector3(0, 0, 0), new Vector3(0, 1, 0), new Vector3(0, 1, 1)  //left
+    };
+
+    private int[] cubeTris =
+    {
+        0, 1, 2,       2, 3, 0,    //front
+        4, 5, 6,       6, 7, 4,    //back
+        8, 9, 10,      10, 11, 8,  //top
+        12, 13, 14,    14, 15, 12, //bot
+        16, 17, 18,    18, 19, 16, //right
+        20, 21, 22,    22, 23, 20  //left
+    };
+
+    private Vector2[] cubeUv =
+    {
+        new Vector2(0, 0), new Vector2(1, 0), new Vector2(0, 1), new Vector2(1, 1),
+        new Vector2(0, 0), new Vector2(1, 0), new Vector2(0, 1), new Vector2(1, 1),
+        new Vector2(0, 0), new Vector2(1, 0), new Vector2(0, 1), new Vector2(1, 1),
+        new Vector2(0, 0), new Vector2(1, 0), new Vector2(0, 1), new Vector2(1, 1),
+        new Vector2(0, 0), new Vector2(1, 0), new Vector2(0, 1), new Vector2(1, 1),
+        new Vector2(0, 0), new Vector2(1, 0), new Vector2(0, 1), new Vector2(1, 1)
+    };
+
+    private Vector3[] cubeNormal =
+    {
+        Vector3.forward, Vector3.forward, Vector3.forward, Vector3.forward,
+        -Vector3.forward, -Vector3.forward, -Vector3.forward, -Vector3.forward,
+        Vector3.up, Vector3.up, Vector3.up, Vector3.up,
+        -Vector3.up, -Vector3.up, -Vector3.up, -Vector3.up,
+        Vector3.right, Vector3.right, Vector3.right, Vector3.right,
+        -Vector3.right, -Vector3.right, -Vector3.right, -Vector3.right
+    };
+
+
 
     private void Start()
     {
-        foreach(Vector3 v in cubeMesh.vertices)
+        foreach(Vector3 v in cubeNormal)
         {
-            Debug.Log(v);
+            //Debug.Log(v);
         }
 
 
 
-        bool[] blocks = new bool[chunkSize * chunkSize * chunkSize];
-
+        bool[] blocks;// = new bool[chunkSize * chunkSize * chunkSize];
+        int counter = 0;
         Vector3 chunkPos;
         for(int x = 0; x < worldSize; ++x)
         {
@@ -31,10 +72,13 @@ public class WorldMaker : MonoBehaviour
             {
                 for (int z = 0; z < worldSize; ++z)
                 {
-
-                    for (int i = 0; i < blocks.Length / 2; ++i)
+                    blocks = new bool[chunkSize * chunkSize * chunkSize];
+                    for (int i = 0; i < blocks.Length; ++i)
                     {
-                        blocks[i] = true;
+                        if (counter % 2 == 0)
+                        {
+                            blocks[i] = true;
+                        }
                     }
 
                     chunkPos.x = x;
@@ -70,24 +114,9 @@ public class WorldMaker : MonoBehaviour
         List<int> triangles;
         List<Vector3> normals;
         List<Vector2> uv;
-
-        //int blockCount = 0;
-        //for (int x = 0; x < chunkSize; ++x)
-        //{
-        //    for (int y = 0; y < chunkSize; ++y)
-        //    {
-        //        for (int z = 0; z < chunkSize; ++z)
-        //        {
-        //            if (blocks[x + (y * chunkSize) + (z * chunkSize * chunkSize)])
-        //            {
-        //                ++blockCount;
-        //            }
-        //        }
-        //    }
-        //}
         
-        int vertCount = cubeMesh.vertices.Length;
-        int triCount = cubeMesh.triangles.Length;
+        int vertCount = cubeVertices.Length;
+        int triCount = cubeTris.Length;
         int faceCount = vertCount / 4;
         
         vertices = new List<Vector3>();
@@ -96,8 +125,6 @@ public class WorldMaker : MonoBehaviour
         normals = new List<Vector3>();
 
         Vector3 blockpos;
-        int blockidx = 0;
-        int faces = 0;
         for (int z = 0; z < chunkSize; ++z)
         {
             for (int y = 0; y < chunkSize; ++y)
@@ -112,62 +139,63 @@ public class WorldMaker : MonoBehaviour
                         
                         for(int i = 0; i < faceCount; ++i)
                         {
-                            switch(i)
+                            switch (i)
                             {
                                 case 0: //front
-                                    if(z < chunkSize - 1
-                                    && blocks[x + (y * chunkSize) + ((z+1) * chunkSize * chunkSize)])
+                                    if(z < chunkSize - 1 && GetBlock(x, y, z + 1, blocks))
                                     {
                                         continue;
                                     }
                                     break;
                                 case 1: //back
-                                    if (z > 0
-                                    && blocks[x + (y * chunkSize) + ((z - 1) * chunkSize * chunkSize)])
+                                    if (z > 0 && GetBlock(x, y, z - 1, blocks))
                                     {
                                         continue;
                                     }
                                     break;
                                 case 2: //top
-
+                                    if (y < chunkSize - 1 && GetBlock(x, y + 1, z, blocks))
+                                    {
+                                        continue;
+                                    }
                                     break;
                                 case 3: //bot
-
+                                    if (y > 0 && GetBlock(x, y - 1, z, blocks))
+                                    {
+                                        continue;
+                                    }
                                     break;
-                                case 4: //left
-
+                                case 4: //right
+                                    if (x < chunkSize - 1 && GetBlock(x + 1, y, z, blocks))
+                                    {
+                                        continue;
+                                    }
                                     break;
-                                case 5: //right
-
+                                case 5: //left
+                                    if (x > 0 && GetBlock(x - 1, y, z, blocks))
+                                    {
+                                        continue;
+                                    }
                                     break;
                                 default:
                                     break;
                             }
-
+                            
                             for(int n = 0; n < 4; ++n)
                             {
-                                vertices.Add(cubeMesh.vertices[n + i * 4] + blockpos);
-                                uv.Add(cubeMesh.uv[n + i * 4]);
-                                normals.Add(cubeMesh.normals[n + i * 4]);
+                                vertices.Add(cubeVertices[n + i * 4] + blockpos);
+                                uv.Add(cubeUv[n + i * 4]);
+                                normals.Add(cubeNormal[n + i * 4]);
                             }
-                            for(int n = 0; n < 6; ++n)
-                            {
-                                triangles.Add(cubeMesh.triangles[n + i * 6] + blockidx * vertCount);
-                            }
+
+                            triangles.Add(vertices.Count - 2);
+                            triangles.Add(vertices.Count - 3);
+                            triangles.Add(vertices.Count - 4);
+                            triangles.Add(vertices.Count - 4);
+                            triangles.Add(vertices.Count - 1);
+                            triangles.Add(vertices.Count - 2);
+                            
                         }
-                        //for (int i = 0; i < vertCount; ++i)
-                        //{
-                        //    vertices.Add(cubeMesh.vertices[i] + blockpos);
-                        //    uv.Add(cubeMesh.uv[i]);
-                        //    normals.Add(cubeMesh.normals[i]);
-                        //}
-
-                        //for (int i = 0; i < triCount; ++i)
-                        //{
-                        //    triangles.Add(cubeMesh.triangles[i] + blockidx * vertCount);
-                        //}
-
-                        ++blockidx;
                     }
                 }
             }
@@ -181,6 +209,11 @@ public class WorldMaker : MonoBehaviour
         mesh.normals = normals.ToArray();
         mesh.uv = uv.ToArray();
         chunkObj.GetComponent<MeshCollider>().sharedMesh = mesh;
+    }
+
+    public bool GetBlock(int x, int y, int z, bool[] blocks)
+    {
+        return blocks[x + y * chunkSize + z * chunkSize * chunkSize];
     }
 
 }
