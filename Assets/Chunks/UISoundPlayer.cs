fa29ffda1b78833602dcd0d740dd8fb6c20c7e3d
Alexander Thomas <6ftDwarf@gmail.com>
2018-04-27 10:54:01 -0700

    Add menu sound effects

---
+++ Assets/Chunks/UISoundPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISoundPlayer : MonoBehaviour {
+
+    public AudioClip normalClick;
+    public AudioClip gameStartClick;
+    public AudioClip sliderNoise;
+
+    public float normalClickVolume;
+    public float sliderNoiseVolume;
+
+    public AudioSource audioSource;
+
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    public void PlayNormalClick()
+    {
+        audioSource.PlayOneShot(normalClick, normalClickVolume);
+    }
+
+    public void PlayGameStartClick()
+    {
+        audioSource.PlayOneShot(gameStartClick);
+    }
+
+    public void PlaySliderNoise()
+    {
+        audioSource.PlayOneShot(sliderNoise, sliderNoiseVolume);
+    }
+
+}
